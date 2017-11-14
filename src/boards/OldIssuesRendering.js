cc9d8cac1870b4ef1553c1fd67da0710ed8b9a37
bobylito <bobylito@gmail.com>
2017-11-14 11:37:59 +0100

    feat: visually hint the freshness of issues

--- src/boards/OldIssuesRendering.js
+++ src/boards/OldIssuesRendering.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
-export default function({data}) {
+import './OldIssues.css';
+
+export default function({ data }) {
   return <ul className="dashboard-lines">{data.map(renderElement)}</ul>;
 }
 
-function renderElement({title, number, link, days}) {
-  return <li className="dashboard-line" key={title}><a href={link}>{title}</a><span className="last-updated">{days}</span></li>;
+function renderElement({ title, number, link, days }) {
+  let howOld;
+  switch (true) {
+    case number < 5:
+      howOld = 'fresh';
+      break;
+    case number < 30:
+      howOld = 'aged';
+      break;
+    default:
+      howOld = 'rotten';
+      break;
+  }
+  const badgeCssClass = `last-updated ${howOld}`;
+  return (
+    <li className="dashboard-line" key={title}>
+      <a href={link}>{title}</a>
+      <span className={badgeCssClass}>{days}</span>
+    </li>
+  );
 }
