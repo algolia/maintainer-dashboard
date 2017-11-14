6980db1d448fc0f0bf8022685b4ffad35ef150d8
vvo <vincent.voyer@algolia.com>
2017-11-14 10:52:19 +0100

    fix(http): move to axios
    got GOT confused in cra (got it?)

--- src/data.js
+++ src/data.js
@@ -1,30 +1,34 @@
 const token = 'd185c09691c9c8a2963d430ab4aebf26bc93bdb7';
-const got = require('got');
+const axios = require('axios');
 const moment = require('moment');
+const gh = axios.create({
+  baseURL: 'https://api.github.com',
+});
 
 module.exports = {
   getOldIssues: () =>
-    got('https://api.github.com/repos/algolia/instantsearch.js/issues', {
-      json: true,
-      query: {
-        direction: 'asc',
-        sort: 'updated',
-        token,
-      },
-    }).then(({ body: issues }) =>
-      issues
-        .filter(
-          issue =>
-            issue.hasOwnProperty('pull_request') === false &&
-            issue.user.login !== 'renovate[bot]'
-        )
-        .map(issue => ({
-          title: issue.title,
-          number: issue.number,
-          link: `https://github.com/algolia/instantsearch.js/issue/${
-            issue.number
-          }`,
-          days: moment(issue.updated_at).fromNow(),
-        }))
-    ),
+    gh
+      .get('/repos/algolia/instantsearch.js/issues', {
+        params: {
+          direction: 'asc',
+          sort: 'updated',
+          token,
+        },
+      })
+      .then(({ data: issues }) =>
+        issues
+          .filter(
+            issue =>
+              issue.hasOwnProperty('pull_request') === false &&
+              issue.user.login !== 'renovate[bot]'
+          )
+          .map(issue => ({
+            title: issue.title,
+            number: issue.number,
+            link: `https://github.com/algolia/instantsearch.js/issue/${
+              issue.number
+            }`,
+            days: moment(issue.updated_at).fromNow(),
+          }))
+      ),
 };
