5f29e4b28ce34c0ba4155097930b6e5f9981d884
bobylito <bobylito@gmail.com>
2017-11-14 11:23:00 +0100

    fix: provide key for component

--- src/boards/OldIssuesRendering.js
+++ src/boards/OldIssuesRendering.js
@@ -5,5 +5,5 @@ export default function({data}) {
 }
 
 function renderElement({title, number, link, days}) {
-  return <li className="dashboard-line"><a href={link}>{title}</a><span className="last-updated">{days}</span></li>;
+  return <li className="dashboard-line" key={title}><a href={link}>{title}</a><span className="last-updated">{days}</span></li>;
 }
