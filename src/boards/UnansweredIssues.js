bfc73e5aa73d723ba2dcaa0f92eacfacf05dfdeb
vvo <vincent.voyer@algolia.com>
2017-11-14 17:01:51 +0100

    link to dashboard rendering

--- src/boards/UnansweredIssues.js
+++ src/boards/UnansweredIssues.js
@@ -4,29 +4,29 @@ import Rendering from './UnansweredIssuesRendering.js';
 import Spinner from '../commons/Spinner.js';
 import Header from './Header.js';
 
-// import gh from '../gh.js';
-function getLastAnswered() {
-  return Promise.resolve([
-    {
-      title: 'Need some help, please',
-      number: '123',
-      link: 'https://github.com/algolia/instantsearch.js/issues/1260',
-      daysSinceLastAnswer: 120,
-    },
-    {
-      title: 'Need some more help, please',
-      number: '124',
-      link: 'https://github.com/algolia/instantsearch.js/issues/1260',
-      daysSinceLastAnswer: 20,
-    },
-    {
-      title: 'Need some help, again',
-      number: '123',
-      link: 'https://github.com/algolia/instantsearch.js/issues/1260',
-      daysSinceLastAnswer: 1,
-    },
-  ]);
-}
+import gh from '../gh.js';
+// function getLastAnswered() {
+//   return Promise.resolve([
+//     {
+//       title: 'Need some help, please',
+//       number: '123',
+//       link: 'https://github.com/algolia/instantsearch.js/issues/1260',
+//       daysSinceLastAnswer: 120,
+//     },
+//     {
+//       title: 'Need some more help, please',
+//       number: '124',
+//       link: 'https://github.com/algolia/instantsearch.js/issues/1260',
+//       daysSinceLastAnswer: 20,
+//     },
+//     {
+//       title: 'Need some help, again',
+//       number: '123',
+//       link: 'https://github.com/algolia/instantsearch.js/issues/1260',
+//       daysSinceLastAnswer: 1,
+//     },
+//   ]);
+// }
 
 export default class UnansweredIssues extends Component {
   constructor(props) {
@@ -36,7 +36,7 @@ export default class UnansweredIssues extends Component {
     };
   }
   componentWillMount() {
-    getLastAnswered().then(data => {
+    gh.getUnansweredIssues().then(data => {
       this.data = data;
       this.setState(() => ({ isDataLoaded: true }));
     });
